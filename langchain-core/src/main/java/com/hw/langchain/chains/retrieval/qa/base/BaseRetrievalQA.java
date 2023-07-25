0b32cfe54f163e33b73bc3bd6d42d122496db3d9
lingjue@ubuntu <penghong@corp.netease.com>
2023-07-25 18:24:36 +0800

    Determine whether the question field exists

--- langchain-core/src/main/java/com/hw/langchain/chains/retrieval/qa/base/BaseRetrievalQA.java
+++ langchain-core/src/main/java/com/hw/langchain/chains/retrieval/qa/base/BaseRetrievalQA.java
@@ -79,7 +79,9 @@ public abstract class BaseRetrievalQA extends Chain {
 
         List<Document> docs = getDocs(question);
         inputs.put("input_documents", docs);
-        inputs.put("question", question);
+        if (!inputs.containsKey("question")) {
+            inputs.put("question", question);
+        }
         String answer = combineDocumentsChain.run(inputs);
 
         Map<String, String> result = Maps.newHashMap();
