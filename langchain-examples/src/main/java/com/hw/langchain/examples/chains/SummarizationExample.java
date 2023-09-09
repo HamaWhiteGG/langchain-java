ad62e7a02bc58a7e81ab6c04b477f848b92bc623
HamaWhiteGG <baisongxx@gmail.com>
2023-09-09 23:28:30 +0800

    Add SummarizationExample

--- langchain-examples/src/main/java/com/hw/langchain/examples/chains/SummarizationExample.java
+++ langchain-examples/src/main/java/com/hw/langchain/examples/chains/SummarizationExample.java
@@ -16,55 +16,36 @@
  * limitations under the License.
  */
 
-package com.hw.langchain.chains.summarize;
+package com.hw.langchain.examples.chains;
 
-import com.hw.langchain.base.language.BaseLanguageModel;
+import com.hw.langchain.chains.summarize.SummarizeUtils;
 import com.hw.langchain.document.loaders.WebBaseLoader;
 import com.hw.langchain.llms.openai.OpenAIChat;
-import com.hw.langchain.schema.Document;
-
-import org.junit.jupiter.api.BeforeAll;
-import org.junit.jupiter.api.Disabled;
-import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static com.hw.langchain.examples.utils.PrintUtils.println;
 
 /**
  * <a href="https://python.langchain.com/docs/use_cases/summarization">Summarization use cases</a>
  *
  * @author HamaWhite
  */
-@Disabled("Test requires costly OpenAI calls, can be run manually.")
-class SummarizeUtilsTest {
+public class SummarizationExample {
 
-    private static BaseLanguageModel llm;
-
-    private static List<Document> docs;
-
-    @BeforeAll
-    static void setUp() {
-        llm = OpenAIChat.builder()
+    public static void main(String[] args) {
+        var llm = OpenAIChat.builder()
                 .temperature(0)
                 .model("gpt-3.5-turbo-16k")
                 .build()
                 .init();
 
         var loader = new WebBaseLoader(List.of("https://lilianweng.github.io/posts/2023-06-23-agent/"));
-        docs = loader.load();
-    }
+        var docs = loader.load();
 
-    @Test
-    void testLoadStuffChain() {
         var chain = SummarizeUtils.loadStuffChain(llm);
-        var actual = chain.run(docs);
+        var result = chain.run(docs);
 
-        var expected =
-                "The article discusses the concept of building autonomous agents powered by large language models " +
-                        "(LLMs). It explores the components of such agents, including planning, memory, and tool " +
-                        "use. The article provides case studies and proof-of-concept examples of LLM-powered agents, " +
-                        "as well as challenges and limitations associated with their development.";
-        assertEquals(expected, actual);
+        println(result);
     }
 }
